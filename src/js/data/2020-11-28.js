81ae4014fbc283e8d08fe4a3a1c1d445ec22e9d1
Vivian <vivianhtlee@gmail.com>
2020-11-29 00:02:22 +0800

    use genshin impact characters

---
+++ src/js/data/2020-11-28.js
@@ -0,0 +1,181 @@
+dataSetVersion = "2020-11-28"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "迪卢克",
+    img: "contentweb/20200609/2020060915105884221.png",
+    opts: {
+    }
+  },
+  {
+    name: "凯亚",
+    img: "contentweb/20191009/2019100910335338023.png",
+    opts: {
+    }
+  },
+  {
+    name: "旅行者-空",
+    img: "https://img-os-static.mihoyo.com/upload/2020/01/15/862bc379e164e9a46689121b7e98dff6.jpeg?x-oss-process=image/resize,s_150/quality,q_80/auto-orient,0/interlace,1/format,jpg",
+    opts: {
+    }
+  },
+  {
+    name: "旅行者-螢",
+    img: "https://img-os-static.mihoyo.com/upload/2020/01/15/862bc379e164e9a46689121b7e98dff6.jpeg?x-oss-process=image/resize,s_150/quality,q_80/auto-orient,0/interlace,1/format,jpg",
+    opts: {
+    }
+  },
+  {
+    name: "派蒙",
+    img: "https://genshin.mihoyo.com/_nuxt/img/37207c1.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "琴",
+    img: "contentweb/20200616/2020061611270460589.png",
+    opts: {
+    }
+  },
+  {
+    name: "安柏",
+    img: "contentweb/20191009/2019100914403137604.png",
+    opts: {
+    }
+  },
+  {
+    name: "丽莎",
+    img: "contentweb/20191009/2019100915145280517.png",
+    opts: {
+    }
+  },
+  {
+    name: "芭芭拉",
+    img: "contentweb/20200609/2020060915134349107.png",
+    opts: {
+    }
+  },
+  {
+    name: "雷泽",
+    img: "contentweb/20191028/2019102814554256644.png",
+    opts: {
+    }
+  },
+  {
+    name: "温迪",
+    img: "contentweb/20191122/2019112211175663364.png",
+    opts: {
+    }
+  },
+  {
+    name: "可莉",
+    img: "contentweb/20200103/2020010311163784222.png",
+    opts: {
+    }
+  },
+  {
+    name: "班尼特",
+    img: "contentweb/20200312/2020031220065363814.png",
+    opts: {
+    }
+  },
+  {
+    name: "诺艾尔",
+    img: "contentweb/20200315/2020031517364710662.png",
+    opts: {
+    }
+  },
+  {
+    name: "菲谢尔",
+    img: "contentweb/20200402/2020040211245736682.png",
+    opts: {
+    }
+  },
+  {
+    name: "砂糖",
+    img: "contentweb/20200723/2020072319191932100.png",
+    opts: {
+    }
+  },
+  {
+    name: "莫娜",
+    img: "contentweb/20200828/2020082818033321128.png",
+    opts: {
+    }
+  },
+  {
+    name: "迪奥娜",
+    img: "contentweb/20201106/2020110619071278706.png",
+    opts: {
+    }
+  },
+  {
+    name: "魈",
+    img: "contentweb/20200220/2020022017115054679.png",
+    opts: {
+    }
+  },
+  {
+    name: "北斗",
+    img: "contentweb/20200227/2020022720190317604.png",
+    opts: {
+    }
+  },
+  {
+    name: "凝光",
+    img: "contentweb/20200305/2020030519452550529.png",
+    opts: {
+    }
+  },
+  {
+    name: "香菱",
+    img: "contentweb/20200316/2020031618254892853.png",
+    opts: {
+    }
+  },
+  {
+    name: "行秋",
+    img: "contentweb/20200324/2020032418184854377.png",
+    opts: {
+    }
+  },
+  {
+    name: "重云",
+    img: "contentweb/20200602/2020060220415594420.png",
+    opts: {
+    }
+  },
+  {
+    name: "七七",
+    img: "contentweb/20200828/2020082817525787679.png",
+    opts: {
+    }
+  },
+  {
+    name: "刻晴",
+    img: "contentweb/20200828/2020082818000226977.png",
+    opts: {
+    }
+  },
+  {
+    name: "达达利亚",
+    img: "contentweb/20201103/2020110321275036932.png",
+    opts: {
+    }
+  },
+  {
+    name: "钟离",
+    img: "contentweb/20201123/2020112310512921570.png",
+    opts: {
+    }
+  },
+  {
+    name: "辛焱",
+    img: "contentweb/20201125/2020112516200273390.png",
+    opts: {
+    }
+  },
+];
